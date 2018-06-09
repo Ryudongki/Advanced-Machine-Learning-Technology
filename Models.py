dd100048dbe1767b4670afb000371b0787d08aae
MakeDirtyCode <make.dirty.code@gmail.com>
2018-06-09 20:50:26 +0900

    train with sphere embed feature

--- Models.py
+++ Models.py
@@ -3,79 +3,71 @@ import network as layer
 
 nc = 3
 nz = 512
+feature_size = 512
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
         
-        options = {'leaky':True, 'bn':True, 'wn':True, 'pixel':True, 'gdrop':True}
+        options = {'leaky':True, 'bn':True, 'wn':False, 'pixel':True, 'gdrop':True}
         
+        self.from_feature = layer.deconv(feature_size, 256, 4, 1, 0, **options)
         # noise
-        self.from_noise = layer.deconv(nz, 512, 4, 1, 0, **options)
+        self.from_noise = layer.deconv(nz, 256, 4, 1, 0, **options)
         # 4 x 4
-        # + 512-vgg-conv
-        self.deconv1 = layer.deconv(1024, 512, 4, 2, 1, **options)
+        # + 256-feature-conv
+        self.deconv1 = layer.deconv(512, 256, 4, 2, 1, **options)
         # 8 x 8
-        # + 512-vgg-conv
-        self.deconv2 = layer.deconv(512, 256, 4, 2, 1, **options)
+        self.deconv2 = layer.deconv(256, 128, 4, 2, 1, **options)
         # 16 x 16
-        # + 256-vgg-conv
-        self.deconv3 = layer.deconv(256, 128, 4, 2, 1, **options)
+        self.deconv3 = layer.deconv(128, 64, 4, 2, 1, **options)
         # 32 x 32
-        # + 128-vgg-conv
-        self.deconv4 = layer.deconv(128, 64, 4, 2, 1, **options)
+        self.deconv4 = layer.deconv(64, nc, 4, 2, 1, gdrop=options['gdrop'], only=True)
         # 64 x 64
-        # + 64-vgg-conv
-        self.deconv5 = layer.deconv(64, nc, 4, 2, 1, gdrop=options['gdrop'], only=True)
-        # 128 x 128
         self.tanh = nn.Tanh()
         
-        self.deconvs = [self.deconv1, self.deconv2, self.deconv3, self.deconv4, self.deconv5]
+        self.deconvs = [self.deconv1, self.deconv2, self.deconv3, self.deconv4]
         
-    def forward(self, x, features):     
-        features = features[::-1]
-        x = self.from_noise(x)        
-        for i in range(len(self.deconvs)):
-            feature, deconv = features[i].detach(), self.deconvs[i]
-            if i < 1:
-                x = torch.cat([x, feature], 1)
+    def forward(self, x, feature):
+        x = self.from_noise(x)
+        feature = self.from_feature(feature.view(-1, 512, 1, 1))
+        x = torch.cat([x, feature], 1)
+        
+        for deconv in self.deconvs:
             x = deconv(x)
         x = self.tanh(x)
+
         return x
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         
-        options = {'leaky':True, 'bn':False, 'wn':True, 'pixel':False, 'gdrop':False}
+        options = {'leaky':True, 'bn':False, 'wn':False, 'pixel':False, 'gdrop':False}
         
-        # 128 x 128
-        self.from_rgb = layer.conv(nc, 64, 4, 2, 1, **options)
+        self.from_feature = layer.linear(feature_size, 64 * 64, leaky=options['leaky'], wn=options['wn'])
+        self.feature_conv = layer.conv(1, 64, 4, 2, 1, **options)
         # 64 x 64
-        # + 64-vgg-conv
-        self.conv1 = layer.conv(64, 128, 4, 2, 1, **options)
+        self.from_rgb = layer.conv(nc, 64, 4, 2, 1, **options)
         # 32 x 32
-        # + 128-vgg-conv
-        self.conv2 = layer.conv(128, 256, 4, 2, 1, **options)
+        self.conv1 = layer.conv(128, 128, 4, 2, 1, **options)
         # 16 x 16
-        # + 256-vgg-conv
-        self.conv3 = layer.conv(256, 512, 4, 2, 1, **options)
+        self.conv2 = layer.conv(128, 256, 4, 2, 1, **options)
         # 8 x 8
-        # + 512-vgg-conv
-        self.conv4 = layer.conv(512, 512, 4, 2, 1, **options)
+        self.conv3 = layer.conv(256, 512, 4, 2, 1, **options)
         # 4 x 4
-        # + 512-vgg-conv
-        self.conv5 = layer.conv(1024, 512, 4, 1, 0, **options)
+        self.conv4 = layer.conv(512, 512, 4, 1, 0, **options)
         # 1 x 1
         self.linear = layer.linear(512, 1, sig=False, wn=options['wn'])
-        self.convs = [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5]
+        self.convs = [self.conv1, self.conv2, self.conv3, self.conv4]
     
-    def forward(self, x, features):
+    def forward(self, x, feature, feature_fake):
         x = self.from_rgb(x)
-        for i in range(len(self.convs)):
-            feature, conv = features[i].detach(), self.convs[i]
-            if i > 3:
-                x = torch.cat([x, feature], 1)
+        feature = self.from_feature(feature).view(-1, 1, 64, 64)
+        feature = self.feature_conv(feature)
+        x = torch.cat([x, feature], 1)
+        
+        for conv in self.convs:
             x = conv(x)
         x = self.linear(x)
         
--- Tester.py
+++ Tester.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 # TODO LIST FROM GANHACKS
 # https://github.com/soumith/ganhacks
 # ✓ Normalize the inputs
@@ -28,13 +22,13 @@ import torchvision.transforms as transforms
 import torch.utils.data
 import torchvision.datasets as dset
 from Folder import ImageFeatureFolder
-
+import net_sphere
 nc = 3
 nz = 512
 lr     = 0.0002
 beta1  = 0.0   
 beta2  = 0.99     
-imageSize = 128
+imageSize = 64
 batchSize = 64
 
 outf = "./celeba_test/"
@@ -70,7 +64,7 @@ from VGG import VGG16Feature
 
 
 netG = Generator()
-vggnet = VGG16Feature()
+feature_net = net_sphere.sphere20a(feature=True)
 
 criterion = nn.MSELoss()
 
@@ -85,8 +79,9 @@ label_fake = torch.FloatTensor(batchSize)
 
 netG.cuda()
 criterion.cuda()
-vggnet.cuda()
-vggnet.eval()
+feature_net.cuda()
+feature_net.load_state_dict(torch.load('./models/sphere20a_20171020.pth'))
+feature_net.eval()
 input, feature, noise = input.cuda(), feature.cuda(), noise.cuda()
 label_real, label_real_smooth, label_fake = label_real.cuda(), label_real_smooth.cuda(), label_fake.cuda()
 fixed_noise = fixed_noise.cuda()
@@ -103,11 +98,24 @@ print()
 # In[5]:
 
 
-netG.load_state_dict(torch.load('./netG_epoch_030.pth'))
+netG.load_state_dict(torch.load('./result/netG_epoch_011.pth'))
 
 
 # In[6]:
 
+feature_input_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.CenterCrop(128), 
+    transforms.Resize(112), 
+    transforms.CenterCrop((112, 96)),
+    transforms.ToTensor()
+])
+
+def transform_batch(batch, transform=feature_input_transform):
+    ret_list = []
+    for i in range(batch.shape[0]):
+        ret_list.append(transform(batch[i]).view(1, 3, 112, 96))
+    return torch.cat(ret_list, 0)
 
 with torch.no_grad():
     for i, data in enumerate(dataloader, 0):
@@ -120,21 +128,19 @@ with torch.no_grad():
         vutils.save_image(input.data,
                 '%s/input.png' % outf,
                 normalize=True)
-        feature = vggnet(inputv)
+        feature_input_real = transform_batch(real_cpu).cuda()
+
+        vutils.save_image(feature_input_real.data, '%s/feature.png' % outf, normalize=True)
+        feature_inputv_real = Variable(feature_input_real)
+        feature_real = feature_net(feature_inputv_real)
+        feature_real = Variable(feature_real.data[7].repeat(64, 1, 1, 1))
         
         noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
         noisev = Variable(noise)
         
-        fake = netG(noisev, feature)
+        fake = netG(noisev, feature_real.detach())
         
         vutils.save_image(fake.data,
                 '%s/output.png' % outf,
                 normalize=True)
         break
-
-
-# In[ ]:
-
-
-
-
